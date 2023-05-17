bf9c3eb51c2dd77fc0d8d69e8ea50f32f6c48582
jamsuham75 <jamsuham75@naver.com>
2023-05-18 07:12:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-// MySQL + nodejs 접속 코드
-var mysql = require("mysql");
-var conn = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "123456",
-    database: "myboard",
-});
 
-conn.connect();
    
 const express = require('express');
 const app = express();
